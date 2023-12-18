a5c7aa1909165bfea6a9b9043ec4802bb3d99f21
Wisch096 <wisch.pg@outlook.com>
2023-12-18 16:26:03 -0300

    O(1)

---
+++ BigO/O(1).cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+    public static void Main(string[] args) {
+        //O(1) -> Tempo constante
+
+        string[] elements = { "apple", "banana", "cherry", "date" };
+        string firstElement = elements[0];
+        
+        Console.WriteLine("First element: " + firstElement);
+    }
+}
