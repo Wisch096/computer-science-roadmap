20eb76826924a32b5de74518a5b641be64a464e7
Wisch096 <wisch.pg@outlook.com>
2023-12-18 15:35:44 -0300

    O(n) -> tempo linear

---
+++ BigO/O(n).cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+
+class Program {
+  public static void Main (string[] args) {
+  //O(n) --> Tempo Linear
+
+        string[] nemo = new string[] { "nemo" };
+        string[] everyone = {"dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"};
+        string[] nemos = Enumerable.Repeat("nemo", 10000).ToArray();
+
+        for (var i = 0; i < nemos.Length; i++) { 
+            if (nemos[i] == "nemo") {
+                Console.WriteLine("Found NEMO!");
+            }
+        }
+    }
+  
+}
